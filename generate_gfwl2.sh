41ba52eb5e0972d24de6249537c8d0e624496817
jingjiezhuang <jingjie.zhuang@igg.com>
2020-01-07 17:53:31 +0800

    Rename gfwl2 source branch gh-pages to archive

--- generate_gfwl2.sh
+++ generate_gfwl2.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
 rm -rf ipblock-ipcn
-git clone --quiet --depth=1 --single-branch --branch gh-pages https://github.com/atmouse-/ipblock-ipcn.git
+git clone --quiet --depth=1 --single-branch --branch archive https://github.com/atmouse-/ipblock-ipcn.git
 if [ $? -ne 0 ]
 then
     exit 1
